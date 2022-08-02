01604703dd4637a13dcf0074d5c970201e78566a
Anas Hameed <hameed.anas@hotmail.com>
2022-08-02 19:00:34 +0500

    Feat: Add a card component

---
+++ components/02-cards/src/App.js
@@ -0,0 +1,12 @@
+import './assets/css/App.css';
+import Card from './components/Card/Card';
+
+function App() {
+  return (
+    <div className="App">
+      <Card></Card>
+    </div>
+  );
+}
+
+export default App;
